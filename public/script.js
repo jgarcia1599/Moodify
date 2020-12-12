caf70d2308634659e240e66de4ffc81fe4adac08
Junior Garcia <junior.f.garcia99@gmail.com>
2020-12-12 16:55:32 +0400

    added styling to frontedn

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 console.log('hiii')
 
-let video;
+let video,canvas;
 
 
 function setup(){
-    createCanvas(640, 480);
+    canvas = createCanvas(640, 480);
+    //Place cavas inside desired DOM element
+	canvas.parent('jumbo-canvas');
     background(255);
     video = createCapture(VIDEO);
     video.hide()
@@ -19,10 +21,21 @@ function draw(){
     console.log('whatuppp');
     faceapi.detectAllFaces(video.elt).withFaceExpressions()
     .then((allFaces) => {
-
+        
         background(255);
         image(video, 640, 0, -640, 480);
-        console.log(allFaces);
+        let moods = allFaces[0]['expressions']
+        console.log(moods);
+        //Get value/per of each moods
+        let angry_per = moods['angry'];
+        let disgusted_per = moods['disgusted'];
+        let happy_per = moods['happy'];
+        let neutral_per = moods['neutral'];
+        let sad_per = moods['sad'];
+        let surprised_per = moods['surprised']
+        //get highest value of mood
+        let highest_val_mood = Object.keys(moods).reduce((a, b) => moods[a] > moods[b] ? a : b)
+        $('#mood_result').text(highest_val_mood);
 
     });
 
