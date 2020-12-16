a7746bd8108f161a6f2ada469553487b831054c3
Junior Garcia <junior.f.garcia99@gmail.com>
2020-12-17 03:22:38 +0400

    a loot of updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort,make_response,jsonify
  
 import sys
 import spotipy
@@ -8,10 +8,9 @@ import random
 
 from moodtape_functions import authenticate_spotify, aggregate_top_artists, aggregate_top_tracks, select_tracks, create_playlist
 
-client_id = "xxx"
-client_secret = "xxx"
-redirect_uri = "xxx"
-
+client_id = "a820c3511f824a918a75d17a862c5ed7"
+client_secret = "737bb17cedcf46349ba3005de0ae25fc"
+redirect_uri = "http://localhost:8888/callback/"
 scope = 'user-library-read user-top-read playlist-modify-public user-follow-read'
 
 # username = "hi18mft50jzkittzkpvgfw9mz"
@@ -37,23 +36,31 @@ def index():
 	username = request.form['username']
 	cache = ".cache-" + username
 	global sp_oauth
+	global access_token
 	sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret,redirect_uri,scope=scope,cache_path=cache )
 	
 	# access_token = ""
 	
 	token_info = sp_oauth.get_cached_token()
-	
+	print(token_info)
 	if token_info:
 		print("Found cached token!")
 		access_token = token_info['access_token']
+		print(access_token)
+		print('================================================')
 		return render_template('index.html') 
 	else:
 		auth_url = sp_oauth.get_authorize_url()
 		return redirect(auth_url)
-	
+@app.route('/results')
+def results():
+	playlist = request.args.get('url')
+	return render_template('playlist.html',playlist = playlist)
+
 @app.route('/callback/')
 def my_callback():
 	global access_token
+	print(access_token)
 	url = request.url
 	
 	code = sp_oauth.parse_response_code(url)
@@ -70,7 +77,12 @@ def my_callback():
 
 @app.route("/moodify", methods=['POST'])
 def moodify():
-	mood = request.form['text']
+	# print(request.json)
+	global access_token
+	print(access_token)
+	print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+	mood = request.json['mood']
 	# username = request.form['username']
 	mood = float(mood)
 	# token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
@@ -79,7 +91,10 @@ def moodify():
 	top_tracks = aggregate_top_tracks(spotify_auth, top_artists)
 	selected_tracks = select_tracks(spotify_auth, top_tracks, mood)
 	playlist = create_playlist(spotify_auth, selected_tracks, mood)
-	return render_template('playlist.html', playlist=playlist )
+	res = make_response(jsonify({
+	"result":playlist
+	}),200)
+	return res
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 console.log('hiii')
-
+let captured = false;
 let video,canvas;
-
+var mood_val = 0.5;
 
 function setup(){
     console.log("inside setup");
@@ -12,77 +12,84 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide()
     faceapi.load
-    faceapi.loadSsdMobilenetv1Model('http://127.0.0.1:8888/static/models/')
-    faceapi.loadFaceExpressionModel('http://127.0.0.1:8888/static/models/');
+    faceapi.loadSsdMobilenetv1Model('http://localhost:8888/static/models/')
+    faceapi.loadFaceExpressionModel('http://localhost:8888/static/models/');
+    $('#buttonstyle').hide();
+
+
 
 }
 
 function draw(){
     // image(video, 0, 0);
     console.log('whatuppp');
-    faceapi.detectAllFaces(video.elt).withFaceExpressions()
-    .then((allFaces) => {
-        console.log(allFaces);
-        if (allFaces.length > 0){
-            background(255);
-            image(video, 640, 0, -640, 480);
-            let moods = allFaces[0]['expressions']
-            console.log(moods);
-            //Get value/per of each moods
-            let angry_per = moods['angry'];
-            let disgusted_per = moods['disgusted'];
-            let happy_per = moods['happy'];
-            let neutral_per = moods['neutral'];
-            let sad_per = moods['sad'];
-            let surprised_per = moods['surprised']
-            //get highest value of mood
-            let highest_val_mood = Object.keys(moods).reduce((a, b) => moods[a] > moods[b] ? a : b)
-            $('#mood_result').text(highest_val_mood);
-        }
-        
-    },
-    (onFailure) => console.log(onFailure));
+        faceapi.detectAllFaces(video.elt).withFaceExpressions()
+        .then((allFaces) => {
+            // console.log(captured);
+            
+
+            if (allFaces.length > 0){
+                $('#waiting').hide()
+                $('#buttonstyle').show()
+                background(255);
+                image(video, 640, 0, -640, 480);
+                let moods = allFaces[0]['expressions']
+                // console.log(moods);
+                //Get value/per of each moods
+                let angry_per = moods['angry'];
+                let disgusted_per = moods['disgusted'];
+                let happy_per = moods['happy'];
+                let neutral_per = moods['neutral'];
+                let sad_per = moods['sad'];
+                let surprised_per = moods['surprised']
+                //get highest value of mood
+                let highest_val_mood = Object.keys(moods).reduce((a, b) => moods[a] > moods[b] ? a : b)
+                $('#mood_result').text(highest_val_mood);
+
+
+                if (highest_val_mood === 'angry')   mood_val = 0.0;
+
+                else if (highest_val_mood === 'disgusted')  mood_val = 0.2
+
+                else if (highest_val_mood === 'happy')  mood_val = 1.0;
+
+                else if (highest_val_mood === 'sad')    mood_val = 0.1;
+
+                else if (highest_val_mood === 'surprised')  mood_val = 0.7;
+            }
+            
+        },
+        (onFailure) => console.log(onFailure));
+
 
 
 }
 
-// Promise.all([
-//     faceapi.loadSsdMobilenetv1Model('models'),
-//     faceapi.loadFaceExpressionModel('models'),
-// ])
-//   .then(startVideo)
-//   .catch(err => console.error(err));
 
-// //Fetching Video frame from HTML
-// const video = document.getElementById('video');
-// //Requesting permission from chrome to access webcam
-// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-// //Fuction which starts streaming og webcam data and creates stream object
-// function startVideo() {
-//   console.log("access");
-//   navigator.getUserMedia(
-//     {
-//       video: {}
-//     },
-//     stream => video.srcObject = stream,
-//     err => console.error(err)
-//   )
-// }
+function makespotifyrequest(){
 
-// video.addEventListener('play', () => {
-// //Creating a canvas to add overlay image
-//   const canvas = faceapi.createCanvasFromMedia(video);
-//   document.body.append(canvas);
-//   const displaySize = { width: video.width, height: video.height };
-//   // faceapi.matchDimensions(canvas, displaySize);
+    console.log("Make Spotify Request; mood val: ",mood_val)
+    document.getElementById("loadercontainer").style.display = "flex";
+    document.getElementById("jumbo-canvas").style.display = "none";
+    fetch('http://localhost:8888/moodify',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({'mood': mood_val})
+    }).then(function(response){
+        response.json().then(function(data){
 
-// //Asynchronusly get detections from the video Stream
-//   setInterval(async () => {
-//     const detections = await faceapi
-//       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()) //Face Detectors
-//       .withFaceExpressions();  //Get Face Expression confidence values
-// // Resize and Display the detections on the video frame using canvas
-// //Printing the detection coordinates
-//     console.log(detections);
-//   }, 100)
-// })
+            url = data['result']
+            window.location = window.origin + "/results" +'?' + 'url='+url
+
+            console.log(data)
+        })
+        // if (response.status== 200){
+        //     window.location = window.origin + "/results" +'?' + 'url='+'sample'
+
+        // }
+    });
+}
+
